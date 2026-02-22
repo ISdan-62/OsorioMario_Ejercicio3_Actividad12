22f6003babd3b1572ab52ec52020b9535214573a
erick <erick@LEGI0SORIO>
2026-02-22 17:04:28 -0600

    “Proyecto creado”

--- Actividad 12-E3/Program.cs
+++ Actividad 12-E3/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("MENU");
+Console.WriteLine("1. Estudiante");
+Console.WriteLine("2. Docente");
+Console.WriteLine("3. Administrativo");
+Console.WriteLine("4. Visitante");
+Console.WriteLine("Seleccionar una opción: ");
+int opcion = int.Parse(Console.ReadLine());
